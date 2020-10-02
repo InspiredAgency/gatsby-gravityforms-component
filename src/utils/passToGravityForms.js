412d1523e003bbfbec36364dd2a131fb60bdd8a2
Daniel Aineah <daniel.aineah@gmail.com>
2020-10-02 18:09:43 +0300

    add fileupload support

--- src/utils/passToGravityForms.js
+++ src/utils/passToGravityForms.js
@@ -1,23 +1,50 @@
 import axios from 'axios'
 
-export default async (baseUrl, formData, lambdaEndpoint) => {
-    let lambaData = {
-        baseUrl: baseUrl,
-        payload: formData,
-    }
+export default async (singleForm, values, lambdaEndpoint) => {
+    const formData = new FormData();
+        for (const attribute in values) {
+            if (values.hasOwnProperty(attribute)) {
+                let value = values[attribute];
+                if (attribute === 'input_2') {
+                    value = value[0]
+                }
+                formData.append(attribute, value);
+            }
+        }
 
     let result
 
     try {
-        result = await axios.post(lambdaEndpoint, lambaData, {
-            responseType: 'json',
-        })
-    } catch (err) {
-        // Pass back error
-        return {
-            status: 'error',
-            data: err.response,
-        }
+        result = await axios.post(`${singleForm.apiURL}/submissions`, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+        });
+    } catch (error) {
+        console.log(error);
+
+    const errorResponse = error.response.data;
+
+    // Here we know this is a Gravity Form Error
+    if (errorResponse.is_valid === false) {
+      return {
+        status: 'error',
+        data: JSON.stringify({
+          status: 'gravityFormErrors',
+          message: 'Gravity Forms has flagged issues',
+          validation_messages: errorResponse.validation_messages,
+        }),
+      };
+    } else {
+      // Unknown error
+      return {
+        status: 'error',
+        data: JSON.stringify({
+          status: 'unknown',
+          message: 'Something went wrong',
+        }),
+      };
+    }
     }
 
     return {
